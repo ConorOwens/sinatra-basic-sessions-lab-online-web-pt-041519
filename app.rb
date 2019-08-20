5c7c5e2c507c4d45e0e01983cf30903dfe7e269c
Conor Owens <conorj.owens@gmail.com>
2019-08-20 03:03:24 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,20 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-
+  
   configure do
     enable :sessions
-    set :session_secret, 'secret'
+    set :session_secret, "secret"
   end
-
+  
   get '/' do
     erb :index
   end
-
-  post '/checkout' do 
+  
+  post '/chekcout' do
     session[:item] = params[:item]
     @session = session
     erb :checkout
   end
-  
+    
 end
